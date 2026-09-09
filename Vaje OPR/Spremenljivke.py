bbdb22be1c2ebf9fc6380937b19d18f883f00b05
Zigacat1 <martincziga@gmail.com>
2026-09-09 06:21:34 +0000

    link

--- Vaje OPR/Spremenljivke.py
+++ Vaje OPR/Spremenljivke.py
@@ -2,3 +2,5 @@ print("Hello world! ♥♦♣♠")
 
 print("Hello")
 print("hii")
+
+# Vaje osnovni tipi in funkcije: https://liascript.github.io/course/?https://raw.githubusercontent.com/Pfyber/zapiskiLIA/main/UPN.md#1
